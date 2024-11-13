e7f478908259f117ca5cd103c50f758a0bfc25b1
jeremeyg <gorre.cjeremy@gmail.com>
2024-11-14 00:15:09 +0800

    add components nav home About add components nav home About

---
+++ real-estate/myapp/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header';
+import About from './components/About';
+
+export default function App() {
+	return (
+		<div className='overflow-hidden'>
+			<Header />
+			<About />
+		</div>
+	);
+}
